3207c3922dc2d62a19737a1ea8ea0ceb92cfa4eb
Thein Htut <theinhtut.dec@gmail.com>
2020-10-23 10:07:09 +0800

    Learn Ch7 Packages

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,51 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order1 = back_of_house::Appetizer::Salad;
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // front_of_house::hosting::add_to_waitlist();
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+    fn cook_order() {}
+}
