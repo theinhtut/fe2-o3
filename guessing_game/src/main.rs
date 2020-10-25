14249bd3884862f74cbe02ab5c176cd6308c1664
Thein Htut <theinhtut.dec@gmail.com>
2020-10-25 21:29:51 +0800

    Learn Ch9 Errors

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,25 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            println!("The secret number will be between 1 and 100.");
+        }
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -17,11 +36,10 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
         println!("You guessed: {}", guess);
     
         match guess.cmp(&secret_number) {
