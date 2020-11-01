dede8e8a6d44ceddd6199610e212a79e46ecb567
Thein Htut <theinhtut.dec@gmail.com>
2020-11-01 17:47:50 +0800

    Added initial minigrep version

---
+++ minigrep/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // Using unwrap_or_else since it returns Config instance
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+
+    // Using if let since run() does not return any value
+    if let Err(e) = minigrep::run(config) {
+        println!("Applaction error: {}", e);
+        process::exit(1);
+    }
+
+}
