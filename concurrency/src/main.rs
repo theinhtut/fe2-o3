581ae3bcdf6147cde1f00d6dcfa5d5eebdab565d
Thein Htut <theinhtut.dec@gmail.com>
2020-12-06 18:14:16 +0800

    Learn Ch16 Fearless Concurrency

---
+++ concurrency/src/main.rs
@@ -0,0 +1,41 @@
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Mutex;
+use std::time::Duration;
+
+fn main() {
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {:?}", m);
+}
+
+fn simple_thread() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
+
+fn mpsc_example() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
