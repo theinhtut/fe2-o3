61bddfd7354935658e6d3615c94400970ea13be0
Thein Htut <theinhtut.dec@gmail.com>
2021-04-01 23:03:55 +0800

    Added mut example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    // let mut x = 5;
-    // println!("The value of x is: {}", x);
-    // x = 6;
-    // println!("The value of x is: {}", x);
+    let mut m = 5;
+    println!("The value of x is: {}", m);
+    m = 6;
+    println!("The value of x is: {}", m);
 
     // Shadowing
     let x = 5;
